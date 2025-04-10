45024024180787b43993bb2f982962387c6f5374
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-04-10 20:21:47 +0530

    Added ParentNode child class of HTMLNode with recursive child handling and unittests

---
+++ src/parentnode.py
@@ -0,0 +1,33 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, props)
+
+        self.children = children
+        if not self.children:
+            raise ValueError("children element(s) is missing")
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("tag element is missing")
+
+        # Begin opening tag
+        full_tag = f"<{self.tag}"
+
+        # Add attributes if props exist
+        if self.props:
+            for key, value in self.props.items():
+                full_tag += f' {key}="{value}"'
+
+        full_tag += ">"  # Close the opening tag
+
+        # Add child HTML recursively
+        for child in self.children:
+            full_tag += child.to_html()
+
+        # Add closing tag
+        full_tag += f"</{self.tag}>"
+
+        return full_tag
---
+++ src/test_parentnode.py
@@ -0,0 +1,44 @@
+import unittest
+
+from leafnode import LeafNode
+from parentnode import ParentNode
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_multiple_grandchildren_and_great_grandchildren(self):
+        great_grandchild_node_1 = LeafNode("b", "great_grandchild_1")
+        great_grandchild_node_2 = LeafNode("p", "great_grandchild_2")
+        grandchild_node_1 = ParentNode("b", [LeafNode(None, "grandchild1")])
+        grandchild_node_2 = ParentNode(
+            "li", [great_grandchild_node_1, great_grandchild_node_2]
+        )
+        child_node = ParentNode("li", [grandchild_node_1, grandchild_node_2])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><li><b>grandchild1</b><li><b>great_grandchild_1</b><p>great_grandchild_2</p></li></li></div>",
+        )
+
+    def test_to_html_with_no_children(self):
+        with self.assertRaises(ValueError) as context:
+            ParentNode("div", []).to_html()
+
+        self.assertEqual(str(context.exception), "children element(s) is missing")
+
+
+if __name__ == "__main__":
+    unittest.main()
