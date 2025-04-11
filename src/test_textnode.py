127eb7af6167b87bc3fd26dea235823e899f3a06
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-04-11 22:34:42 +0530

    Added function to convert TextNodes to HTMLNodes and unittests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -45,6 +45,55 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, "")
         self.assertNotEqual(node, ["", TextType.NORMAL])
         self.assertNotEqual(node, 69420)
+
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_italic(self):
+        node = TextNode("This is a italic text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a italic text node")
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertEqual(html_node.props, {"href": "https://boot.dev"})
+
+    def test_image_link(self):
+        node = TextNode(
+            "Boots smiling",
+            TextType.IMAGE_LINK,
+            "https://blog.boot.dev/news/introducing-boots-ai-code-explainer",
+        )
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(
+            html_node.props,
+            {
+                "src": "https://blog.boot.dev/news/introducing-boots-ai-code-explainer",
+                "alt": "Boots smiling",
+            },
+        )
 
 
 if __name__ == "__main__":
