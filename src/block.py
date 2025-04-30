821e15c8daa54fffb27ed9f4b51c80ffc73d38f8
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-04-30 13:11:27 +0530

    Added BlockType enum, block_to_block_type function with unittests

--- src/block.py
+++ src/block.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     split_markdown = markdown.split("\n\n")
 
@@ -6,3 +18,48 @@ def markdown_to_blocks(markdown):
     )
 
     return filtered_markdown
+
+
+def block_to_block_type(text):
+    lines = text.split("\n")
+    count = 0
+
+    # Heading check
+    while count < len(text) and text[count] == "#":
+        count += 1
+    if 1 <= count <= 6 and count < len(text) and text[count] == " ":
+        return BlockType.HEADING
+
+    # Code check
+    if len(lines) > 1 and lines[0] == "```" and lines[-1] == "```":
+        return BlockType.CODE
+
+    # Quote check
+    for line in lines:
+        if not line.startswith(">"):
+            break
+    else:
+        return BlockType.QUOTE
+
+    # Unordered list check
+    for line in lines:
+        if not line.startswith("- "):
+            break
+    else:
+        return BlockType.UNORDERED_LIST
+
+    # Ordered list check
+    current_line_number = 1
+
+    for line in lines:
+        if not line.startswith(f"{current_line_number}. "):
+            break
+        current_line_number += 1
+    else:
+        return BlockType.ORDERED_LIST
+
+    """
+    If the text is not any of the type above it is paragraph by default
+    according to assignment.
+    """
+    return BlockType.PARAGRAPH
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -48,6 +48,119 @@ There are **two** blank lines above and **one** below this.
         markdown = ""
         blocks = markdown_to_blocks(markdown)
         self.assertListEqual([], blocks)
+
+    def test_block_to_block_type_heading(self):
+        text = "##### Heading5"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_near_miss(self):
+        text = "###Missing Space After '#'"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_heading_extra_hash(self):
+        text = "####### Not valid heading"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_quote(self):
+        text = """> First quote
+>Second quote"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_block_type_quote_near_miss(self):
+        text = """
+> First quote
+
+>Second quote
+"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_code(self):
+        text = """```
+print(hello)
+```"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_code_near_miss(self):
+        text = """```
+print(hello)
+``"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_unordered_list(self):
+        text = """
+- item one
+- item two
+- item three
+""".strip()
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_unordered_list_near_miss(self):
+        text = """
+- item one
+- item two
+-item three
+"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_ordered_list(self):
+        text = """
+1. item one
+2. item two
+3. item three
+""".strip()
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_block_to_block_type_ordered_list_near_miss(self):
+        text = """
+1. item one
+2. item two
+4. item four
+""".strip()
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph(self):
+        text = "Lorem Ipsum"
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph_multiline(self):
+        text = """
+First Line
+Second Line
+Third Line
+"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph_mixed(self):
+        text = """
+1. ordered
+- unordered
+# heading
+>quote
+lorem ipsum
+"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    # Recommended by Boots
+    def test_block_to_block_type_heading_inside_code_block(self):
+        text = """```
+### Heading inside code block
+```"""
+        block_type = block_to_block_type(text)
+        self.assertEqual(block_type, BlockType.CODE)
 
 
 if __name__ == "__main__":
